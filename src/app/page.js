47f267b7337ea1970e97edc82f122bf4177949a8
Milesm <miles1301@icloud.com>
2025-01-15 21:03:07 -0800

    checking commits working

--- src/app/page.js
+++ src/app/page.js
@@ -20,7 +20,7 @@ export default function Home() {
             </code>
             .
           </li>
-          <li>Save and see your changes instantly1!!!!?</li>
+          <li>Save and see your changes instantly123333!!!!?</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
